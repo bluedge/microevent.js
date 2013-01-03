1180b6d8d2461b0394acab0f2a38b94a19bd874c
bluedge <n.alessandra@gmail.com>
2013-01-03 18:25:58 +0100

    Update microevent-debug.js

--- microevent-debug.js
+++ microevent-debug.js
@@ -6,19 +6,19 @@
 
 var MicroEvent	= function(){}
 MicroEvent.prototype	= {
-	bind	: function(event, fct){
+	on	: function(event, fct){
 		this._events = this._events || {};		
 		this._events[event] = this._events[event]	|| [];
 		this._events[event].push(fct);
 	},
-	unbind	: function(event, fct){
+	removeListener	: function(event, fct){
 		console.assert(typeof fct === 'function');
 		this._events = this._events || {};		
 		if( event in this._events === false  )	return;
 		console.assert(this._events[event].indexOf(fct) !== -1);
 		this._events[event].splice(this._events[event].indexOf(fct), 1);
 	},
-	trigger	: function(event /* , args... */){
+	emit	: function(event /* , args... */){
 		this._events = this._events || {};		
 		if( event in this._events === false  )	return;
 		for(var i = 0; i < this._events[event].length; i++){
@@ -35,7 +35,7 @@ MicroEvent.prototype	= {
  * @param {Object} the object which will support MicroEvent
 */
 MicroEvent.mixin	= function(destObject){
-	var props	= ['bind', 'unbind', 'trigger'];
+	var props	= ['on', 'removeListener', 'emit'];
 	for(var i = 0; i < props.length; i ++){
 		destObject.prototype[props[i]]	= MicroEvent.prototype[props[i]];
 	}
